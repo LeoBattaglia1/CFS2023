454c6b16c200389c3241f3a382b7888e13420a64
Leandro Battaglia <102628431+LeoBattaglia1@users.noreply.github.com>
2023-07-06 15:00:51 -0300

    Delete concesionaria directory

--- concesionaria/src/concesionaria/concesionaria.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConcesionariaController } from './concesionaria.controller';
-import { ConcesionariaService } from './concesionaria.service';
-
-@Module({
-    controllers: [ConcesionariaController],
-    providers: [ConcesionariaService]
-  })
-export class ConcesionariaModule {}
